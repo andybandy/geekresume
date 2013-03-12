49b3d47aec1c7aa02d68eebe959cc63863566423
Artyom <art.key89@gmail.com>
2013-03-13 00:45:59 +0400

    Отображение резюме в show action

--- geekresume/app/models/resume.rb
+++ geekresume/app/models/resume.rb
@@ -2,6 +2,8 @@
 # Обаладает свойством название, которое должно быть уникально в рамках
 # одного пользователя.
 # После создания нового резюме создается пустой гит репозиторий
+# Resume#has_content? - проверяет есть ли файл с резюме в репе
+# Resume#content_html - html формат резюме
 class Resume < ActiveRecord::Base
   attr_accessible :title, :user
   belongs_to :user
@@ -18,6 +20,10 @@ class Resume < ActiveRecord::Base
     @namespace ||= "artkey/#{title.parameterize}"
   end
 
+  def path_with_namespace
+    namespace
+  end
+
   def path_to_repo
     @path_to_repo ||= File.join(Settings.repos_path, "#{namespace}.git")
   end
@@ -27,7 +33,11 @@ class Resume < ActiveRecord::Base
   end
 
   def content
-    (repo.tree / "resume.md").data
+    (repo.tree / "resume.md").try(:data)
+  end
+
+  def has_content?
+    content
   end
 
   private
@@ -35,5 +45,9 @@ class Resume < ActiveRecord::Base
   def init_bare_repo
     Grit::Repo.init_bare path_to_repo
   end
+
+end
+
+class Repository
 
 end
