4aa0421dce91963f7477169b9168e264b1ce54d7
Artyom <art.key89@gmail.com>
2013-03-16 17:34:01 +0400

    Оформлены flash сообщения

--- geekresume/app/helpers/application_helper.rb
+++ geekresume/app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
+# encoding: utf-8
 module ApplicationHelper
+
+  # Overwrite base devise method
+  def error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+      <div id="error_explanation">
+      <ul>#{messages}</ul>
+      </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def flash_message type, message
+      content_tag :div, class: type do
+        (content_tag :button, "×", class: :close, data: { dismiss: "alert" }) + 
+        message.html_safe
+      end
+  end
+
 end
