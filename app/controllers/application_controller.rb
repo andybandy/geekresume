17e46eb35d4d64e48268f22a7345f9c8b9f5b6e8
Artyom Keydunov <artyom.keydunov@gmail.com>
2013-07-15 19:58:23 +0400

    Strong params in sign_in and sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         authentication_keys: [:email, :username]
 
   validates :username, presence: true, uniqueness: true
   has_many :resumes
