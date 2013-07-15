9b63a5defc2bf27aebee0e65711be04709ee9c70
Artyom Keydunov <artyom.keydunov@gmail.com>
2013-07-15 13:55:56 +0400

    Change production settings as in rails 4

--- config/application.rb
+++ config/application.rb
@@ -48,8 +48,5 @@ module Geekresume
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
   end
 end
